91a255644dbd080b4f070f533cad56f19727d270
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-06-04 15:00:48 +0530

    feat: add main app file

---
+++ index.js
@@ -0,0 +1,59 @@
+// Load logger
+import Winston from './utils/winston';
+const log = new Winston('Main');
+
+// Load environment variables
+import('dotenv')
+  .then((dotenv) => dotenv.config())
+  .catch((error) => log.error(`Could not load environemnt variables: `, error));
+
+// Import all modules
+import express from 'express';
+import { init as initMongoose } from './config/mongoose';
+import { init as initMysql } from './config/mysql';
+import { migrateMany as migrateManyUsers, migrateSingle as migrateSingleUser } from './controllers/users';
+
+log.info(`Initializing Mongoose...`);
+initMongoose();
+
+log.info(`Initializing MySQL...`);
+initMysql();
+
+log.info(`Initializing Express...`);
+const app = express();
+
+async function requestHandler(methodResponse, sendResponse) {
+  const _res = await methodResponse;
+  if (!_res) {
+    sendResponse.status(500);
+    return sendResponse.json({
+      status: 500,
+      message: 'There were errors. See logs for more information.',
+      data: _res,
+    });
+  }
+  sendResponse.status(200);
+  return sendResponse.json({
+    status: 200,
+    message: 'All tasks completed sucessfully. See logs for more information.',
+    data: _res,
+  });
+}
+
+/**
+ * User Migration Endpoints
+ */
+app.use('/users/migrate/single/:userId', (req, res) =>
+  requestHandler(migrateSingleUser(parseInt(req.params.userId)), res)
+);
+
+app.use('/users/migrate/many/:startId/:endId', (req, res) =>
+  requestHandler(migrateManyUsers(req.params.startId, req.params.endId), res)
+);
+
+const server = app.listen(process.env.PORT || 8080, (error) => {
+  if (error) return log.error(`Could not start server: `, error);
+  return log.info(`Server started listening on 0.0.0.0:${process.env.PORT || 8080}`);
+});
+
+export default server;
