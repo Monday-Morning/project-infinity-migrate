cb60823d80d98fa58d405ecc663dfbf1343f6d07
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-06-04 16:54:12 +0530

    feat: add catch all route

--- index.js
+++ index.js
@@ -49,6 +49,15 @@ app.use('/users/migrate/many/:startId/:endId', (req, res) =>
   requestHandler(migrateManyUsers(req.params.startId, req.params.endId), res)
 );
 
+// Catch All
+app.use((req, res) =>
+  res.status(404).json({
+    status: 404,
+    message: 'Requested page does not exist.',
+    data: req.url,
+  })
+);
+
 const server = app.listen(process.env.PORT || 8080, (error) => {
   if (error) return log.error(`Could not start server: `, error);
   return log.info(`Server started listening on 0.0.0.0:${process.env.PORT || 8080}`);
